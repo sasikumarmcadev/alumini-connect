6b0593c0cd3b907691e19c41c9ed8e475fda4e25
Sasikumar072004 <sasi93602075@gmail.com>
2025-12-07 11:14:18 +0530

    Fix DJANGO_SETTINGS_MODULE to api.settings

--- api/wsgi.py
+++ api/wsgi.py
@@ -11,5 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news_aggregator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 application = get_wsgi_application()
